861284c1cfac82fd08849aa4d9f7890fdfc2778f
Prakhar Chaudhary <prakharchaudhary0302@gmail.com>
2026-03-01 16:00:43 +0530

    GlobalExceptionHandler added

--- src/main/java/com/jobportal/srm/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jobportal/srm/exception/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.jobportal.srm.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice // Makes this class handle exceptions globally
 public class GlobalExceptionHandler {
+
+    // Handles all RuntimeExceptions thrown in application
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+
+        return ResponseEntity
+                .badRequest() // Returns HTTP 400
+                .body(ex.getMessage()); // Sends exception message as response body
+    }
 }
